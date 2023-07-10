ee01df8a89e3534ca0502168431220ed03fa94af
Mihail Nephedov <Nephedov93@gmail.com>
2023-07-10 05:40:24 +0300

    Add allure

--- app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/MainActivityTextTest.java
+++ app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/MainActivityTextTest.java
@@ -9,15 +9,16 @@ import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
 import androidx.test.espresso.ViewInteraction;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
-import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import io.qameta.allure.android.runners.AllureAndroidJUnit4;
+
 @LargeTest
-@RunWith(AndroidJUnit4.class)
+@RunWith(AllureAndroidJUnit4.class)
 public class MainActivityTextTest {
 
     @Rule
